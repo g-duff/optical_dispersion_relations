4471e0a42c026510829d6c69d99764e5759f5163
George Duffett <george.duffett@me.com>
2020-05-19 00:26:02 +0100

    Added matrix method of field coefficients

--- field_plot.py
+++ field_plot.py
@@ -6,6 +6,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import mim_dispersions as dsp
 import cmath as cm
+import numpy.linalg as la
 
 font = {'size':16}
 matplotlib.rc('font', **font)
@@ -13,7 +14,7 @@ matplotlib.rc('font', **font)
 ## Wavelength Insulator RI and thickness
 wl = 500
 i_n = 1.45
-t_i = 600
+t_i = 20
 
 ## Metal refractive index and extinction coefficient
 m_wl, m_n, = np.genfromtxt('./material_dispersions/ag_n.txt',
@@ -36,10 +37,11 @@ m_k = griddata(m_wl, m_k, wl, method='cubic')
 m_eps = (m_n + 1j*m_k)**2
 i_eps = i_n**2
 
+# m_eps, i_eps = i_eps, m_eps
+
 ## Calcualte spp neff
 spp_n_eff = dsp.spp_neff(i_eps, m_eps)
 spp_beta = spp_n_eff*k0
-
 
 # Calculate SPP field
 k1 = cm.sqrt(spp_beta**2 - k0**2*i_eps)
@@ -49,31 +51,40 @@ xm = np.arange(-3*t_i, -t_i/2,)
 ep = np.exp(-k1*(xp+t_i/2))*(np.exp(k1*(t_i/2)) + np.exp(-1*k1*(t_i/2)))
 em = np.exp(k2*(xm+t_i/2))*(np.exp(k1*(t_i/2)) + np.exp(-1*k1*(t_i/2)))
 
-fig, ax = plt.subplots()
-ax.plot(xp, ep)
-ax.plot(xm, em)
+# fig, ax = plt.subplots()
+# ax.plot(xp, ep)
+# ax.plot(xm, em)
 # plt.show()
 
 # Newton-Raphson process, decreasing gap thickness
-beta = opt.newton(dsp.mim_disp, spp_beta, args=(k0, m_eps, i_eps, t_i),
-	maxiter=int(1e6), tol=1e3)
+beta = opt.newton(dsp.mim_disp, spp_beta, args=(k0, m_eps, i_eps, t_i, True),
+	maxiter=int(1e6), tol=1e-30)
 n_eff =  beta.real/k0
 
 # Calculate MIM field
-k2, k1 =  dsp.mim_disp(beta, k0, m_eps, i_eps, t_i, full_output=True)
+k1, k2 =  dsp.mim_disp(beta, k0, m_eps, i_eps, t_i, True, full_output=True)
 
 x1 = np.arange(t_i/2, 3*t_i)
 x2 = np.arange(-t_i/2, t_i/2)
 x3 = np.arange(-3*t_i, -t_i/2)
 
-e1 = (np.exp(k1*(t_i/2)) + np.exp(-1*k1*(t_i/2)))*np.exp(-k2*(x1-t_i/2))
-e2 = np.exp(k1*(x2)) + np.exp(-1*k1*(x2))
-e3 = (np.exp(k1*(-t_i/2)) + np.exp(-1*k1*(-t_i/2)))*np.exp(k2*(x3+t_i/2))
+# Find the boundary conditions
+A = 1
+[C, D] = la.inv(np.array([
+	[np.exp(k2*t_i/2), np.exp(-1*k2*t_i/2)],
+	[-1j*k2*np.exp(k2*t_i/2)/i_eps, 1j*k2*np.exp(-k2*t_i/2)/i_eps]
+])) @ np.array([np.exp(-k1*t_i/2), 1j*k1*np.exp(-k1*t_i/2)/m_eps])
+B = (C*np.exp(-k2*t_i/2) + D*np.exp(k2*t_i/2))/np.exp(-k1*t_i/2)
+
+# Calculate the fields, as in Maier
+e1 = A*np.exp(-k1*x1)
+e2 = C*np.exp(k2*x2) + D*np.exp(-1*k2*x2)
+e3 = B*np.exp(k1*x3)
 
 mim_x = np.concatenate([x1, x2, x3])
 mim_y = np.concatenate([e1, e2, e3])
 
-# fig, ax = plt.subplots()
-ax.plot(mim_x, mim_y.real, '.', ms=1)
-ax.plot(mim_x, mim_y.imag, '.', ms=1)
+fig, ax = plt.subplots()
+ax.plot(mim_x, mim_y.real, 'C3.', ms=3)
+ax.plot(mim_x, mim_y.imag, 'k.', ms=3)
 plt.show()
--- field_plot_2d.py
+++ field_plot_2d.py
@@ -6,6 +6,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import mim_dispersions as dsp
 import cmath as cm
+import numpy.linalg as la
 
 font = {'size':16}
 matplotlib.rc('font', **font)
@@ -36,10 +37,56 @@ m_k = griddata(m_wl, m_k, wl, method='cubic')
 m_eps = (m_n + 1j*m_k)**2
 i_eps = i_n**2
 
+# m_eps, i_eps = i_eps, m_eps
+
 ## Calcualte spp neff
 spp_n_eff = dsp.spp_neff(i_eps, m_eps)
 spp_beta = spp_n_eff*k0
 
+# Calculate SPP field
+k1 = cm.sqrt(spp_beta**2 - k0**2*i_eps)
+k2 = cm.sqrt(spp_beta**2 - k0**2*m_eps)
+xp = np.arange(-t_i/2, t_i/2)
+xm = np.arange(-3*t_i, -t_i/2,)
+ep = np.exp(-k1*(xp+t_i/2))*(np.exp(k1*(t_i/2)) + np.exp(-1*k1*(t_i/2)))
+em = np.exp(k2*(xm+t_i/2))*(np.exp(k1*(t_i/2)) + np.exp(-1*k1*(t_i/2)))
+
+# Newton-Raphson process, decreasing gap thickness
+beta = opt.newton(dsp.mim_disp, spp_beta, args=(k0, m_eps, i_eps, t_i, False),
+	maxiter=int(1e6), tol=1e-20)
+n_eff =  beta.real/k0
+
+# Calculate MIM field
+k1, k2 =  dsp.mim_disp(beta, k0, m_eps, i_eps, t_i, False, full_output=True)
+
+# Find the boundary conditions
+A = 1
+[C, D] = la.inv(np.array([
+	[np.exp(k2*t_i/2), np.exp(-1*k2*t_i/2)],
+	[-1j*k2*np.exp(k2*t_i/2)/i_eps, 1j*k2*np.exp(-k2*t_i/2)/i_eps]
+])) @ np.array([np.exp(-k1*t_i/2), 1j*k1*np.exp(-k1*t_i/2)/m_eps])
+B = (C*np.exp(-k2*t_i/2) + D*np.exp(k2*t_i/2))/np.exp(-k1*t_i/2)
+
+x1_coords = np.arange(t_i/2, 3*t_i)
+x2_coords = np.arange(-t_i/2, t_i/2)
+x3_coords = np.arange(-3*t_i, -t_i/2)
+z_coords = np.arange(0, 3000, 30)
+z1_grid, x1_grid = np.meshgrid(z_coords, x1_coords)
+z2_grid, x2_grid = np.meshgrid(z_coords, x2_coords)
+z3_grid, x3_grid = np.meshgrid(z_coords, x3_coords)
+
+# Calculate the fields, as in Maier
+e1 = (A*np.exp(-k1*x1_grid))*np.exp(-1j*beta*z1_grid)
+e2 = (C*np.exp(k2*x2_grid) + D*np.exp(-1*k2*x2_grid))*np.exp(-1j*beta*z2_grid)
+e3 = (B*np.exp(k1*x3_grid))*np.exp(-1j*beta*z3_grid)
+
+x_coords = np.concatenate([x3_coords, x2_coords, x1_coords])
+mim_y = np.concatenate([e3, e2, e1])
+
+fig, ax = plt.subplots()
+cb = ax.pcolormesh(z_coords, x_coords, mim_y.real)
+fig.colorbar(cb)
+plt.show()
 
 # Calculate SPP field
 # k1 = cm.sqrt(spp_beta**2 - k0**2*i_eps)
@@ -54,38 +101,3 @@ spp_beta = spp_n_eff*k0
 # ax.plot(xm, em)
 
 # plt.show()
-
-# Newton-Raphson process, decreasing gap thickness
-mim_beta = opt.newton(dsp.mim_disp, spp_beta, args=(k0, m_eps, i_eps, t_i),
-	maxiter=int(1e6), tol=1e3)
-n_eff =  mim_beta/k0
-
-print(f'MIM n_eff: {n_eff}')
-
-# Calculate MIM field
-k2, k1 =  dsp.mim_disp(mim_beta, k0, m_eps, i_eps, t_i, full_output=True)
-
-x1_coords = np.arange(t_i/2, 3*t_i/2)
-x2_coords = np.arange(-t_i/2, t_i/2)
-x3_coords = np.arange(-3*t_i/2, -t_i/2)
-
-z_coords = np.arange(0, 500, 1)
-z1_grid, x1_grid = np.meshgrid(z_coords, x1_coords)
-z2_grid, x2_grid = np.meshgrid(z_coords, x2_coords)
-z3_grid, x3_grid = np.meshgrid(z_coords, x3_coords)
-
-boundary = (np.exp(k1*(t_i/2)) + np.exp(-1*k1*(t_i/2)))
-h1 = (boundary*np.exp(-k2*(x1_grid-t_i/2)))*np.exp(1j*mim_beta*z1_grid)
-h2 = (np.exp(k1*(x2_grid)) + np.exp(-1*k1*(x2_grid)))*np.exp(1j*mim_beta*z2_grid)
-h3 = (boundary*np.exp(k2*(x3_grid+t_i/2)))*np.exp(1j*mim_beta*z3_grid)
-
-H = np.concatenate((h3, h2, h1))
-x_coords = np.concatenate((x3_coords, x2_coords, x1_coords))
-
-fig, ax = plt.subplots()
-
-ax.pcolormesh(z_coords, x_coords, H.real)
-ax.axhline(t_i/2, color='w', ls='-')
-ax.axhline(-t_i/2, color='w', ls='-')
-
-plt.show()
