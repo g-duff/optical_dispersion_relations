d9766128befca916d625e9f870fa65c4e47e652e
george <george.duffett@icloud.com>
2022-11-11 19:00:03 +0000

    Add single pole model

--- src/dispersion_relations/drude_lorentz.py
+++ src/dispersion_relations/drude_lorentz.py
@@ -1,6 +1,32 @@
 '''Drude Lorentz Dispersion Relations'''
 
 
+def single_pole(angular_frequency,
+                plasma_frequency,
+                damping_rate,
+                dielectric_constant=1,
+                resonance_angular_frequency=0
+                ):
+    '''Single Pole Drude-Lorentz Dispersion Relation, for use with eg Silver
+
+    Parameters:
+        angular_frequency: the angular frequency at which to calculate the permittivity
+        plasma_frequency: natural frequency of a free oscillation of the electron sea
+        damping_rate: characteristic collision frequency of the metal
+        dielectric_constant: offset permittivity due to positive ion cores
+        resonance_angular_frequency: the Lorentz oscillator peak frequency
+
+    Returns:
+        Complex permittivity at the specified angular_frequency
+    '''
+    permittivity = dielectric_constant - plasma_frequency**2 * lorentz_oscillator(
+        angular_frequency,
+        resonance_angular_frequency,
+        damping_rate,
+    )
+    return permittivity
+
+
 def lorentz_oscillator(angular_frequency,
                        resonance_angular_frequency,
                        damping_rate,
