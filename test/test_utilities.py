96feb6dcc16c643cc703355709f939d2cb72faff
george <george.duffett@icloud.com>
2022-11-13 22:38:33 +0000

    Add refractive index to permittivity test

--- test/test_utilities.py
+++ test/test_utilities.py
@@ -1,3 +1,22 @@
 # pylint: disable = import-error, missing-class-docstring, missing-function-docstring, missing-module-docstring
 import unittest
 from src.dispersion_relations import utilities
+
+
+class Utilities(unittest.TestCase):
+
+    def test_refractive_index_to_permittivity(self):
+        # Given
+        refractive_index = 2.0+0.5j
+        expected_permittivity_real = 3.75
+        expected_permittivity_imaginary = 2.0
+
+        # When
+        actual_permittivity = utilities.refractive_index_to_permittivity(
+            refractive_index)
+
+        # Then
+        self.assertAlmostEqual(expected_permittivity_real,
+                               actual_permittivity.real)
+        self.assertAlmostEqual(
+            expected_permittivity_imaginary, actual_permittivity.imag)
