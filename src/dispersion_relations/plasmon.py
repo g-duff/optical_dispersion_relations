3488e19c9537801c72354b0f500ec2f7246097e8
george <george.duffett@icloud.com>
2022-11-13 19:53:47 +0000

    Add surface plasmon polariton dispersion

---
+++ src/dispersion_relations/plasmon.py
@@ -0,0 +1,22 @@
+'''Plasmonics Dispersion Relations'''
+import numpy as np
+
+def surface_plasmon_polariton(dielectric_permittivity, metal_permittivity):
+    '''Exact surface plasmon dispersion relation for TM polarization.
+    Surface plasmons only exist for TM polarization.
+
+    Parameters:
+        dielectric_permittivity: float, complex number or numpy array
+        metal_permittivity: float, complex number or numpy array
+
+    Returns:
+        effective_refractive_index of surface plasmon polariton: complex number or numpy array
+
+    Derivation in:
+    Maier SA. Plasmonics: fundamentals and applications.
+    ISBN: 978-0-387-37825-1
+    '''
+    numerator = dielectric_permittivity*metal_permittivity
+    denominator = dielectric_permittivity+metal_permittivity
+    effective_refractive_index = np.sqrt(numerator/denominator)
+    return effective_refractive_index
