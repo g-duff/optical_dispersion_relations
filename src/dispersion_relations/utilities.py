22461cdada1e9caca76a31f213847aa40ed25e20
george <george.duffett@icloud.com>
2022-11-13 22:39:09 +0000

    Add permittivity to refractive index function

--- src/dispersion_relations/utilities.py
+++ src/dispersion_relations/utilities.py
@@ -2,6 +2,20 @@
 import numpy as np
 
 
+def permittivity_to_refractive_index(
+    permittivity: complex
+) -> complex:
+    '''Convert complex permittivity to complex refractive index
+
+    Parameters:
+        permittivity
+
+    Returns:
+        complex refractive index
+    '''
+    return np.sqrt(permittivity)
+
+
 def refractive_index_to_permittivity(
     refractive_index: complex
 ) -> complex:
