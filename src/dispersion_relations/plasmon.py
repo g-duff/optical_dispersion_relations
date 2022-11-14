6a0fd7536bbb91aba15effc4c2c1ccab184fc95c
george <george.duffett@icloud.com>
2022-11-14 21:46:59 +0000

    Change spp documentation whitespace

--- src/dispersion_relations/plasmon.py
+++ src/dispersion_relations/plasmon.py
@@ -14,8 +14,8 @@ def surface_plasmon_polariton(dielectric_permittivity, metal_permittivity):
         effective_refractive_index of surface plasmon polariton: complex number or numpy array
 
     Derivation in:
-    Maier SA. Plasmonics: fundamentals and applications.
-    ISBN: 978-0-387-37825-1
+        Maier SA. Plasmonics: Fundamentals and Applications.
+        ISBN: 978-0-387-37825-1
     '''
     numerator = dielectric_permittivity*metal_permittivity
     denominator = dielectric_permittivity+metal_permittivity
