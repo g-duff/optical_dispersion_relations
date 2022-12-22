41f9d81c8a6c2abf2d49d548964053a95490954d
george <george.duffett@icloud.com>
2022-12-22 18:44:03 +0000

    Fix wavelength unit in plot

--- examples/scripts/drude_lorentz_example.py
+++ examples/scripts/drude_lorentz_example.py
@@ -64,27 +64,27 @@ if __name__ == '__main__':
     fig, (real_part_axes, imaginary_part_axes) = plt.subplots(
         ncols=2, figsize=(8, 4))
 
-    real_part_axes.plot(wavelengths, silver_permittivity.real,
+    real_part_axes.plot(wavelengths/const.nano, silver_permittivity.real,
                         'C0-', label='Silver')
-    real_part_axes.plot(silver_wavelengths, silver_empirical_permittivity.real,
+    real_part_axes.plot(silver_wavelengths/const.nano, silver_empirical_permittivity.real,
                         'C0o')
-    real_part_axes.plot(wavelengths, gold_permittivity.real,
+    real_part_axes.plot(wavelengths/const.nano, gold_permittivity.real,
                         'C1-', label='Gold')
-    real_part_axes.plot(gold_wavelengths, gold_empirical_permittivity.real,
+    real_part_axes.plot(gold_wavelengths/const.nano, gold_empirical_permittivity.real,
                         'C1o')
-    real_part_axes.set_xlabel('Free space wavelength (m)')
+    real_part_axes.set_xlabel('Free space wavelength (nm)')
     real_part_axes.set_ylabel('Permittivity, real part')
     real_part_axes.legend()
 
     imaginary_part_axes.plot(
-        wavelengths, silver_permittivity.imag, 'C0-',  label='Silver')
-    imaginary_part_axes.plot(silver_wavelengths, silver_empirical_permittivity.imag,
+        wavelengths/const.nano, silver_permittivity.imag, 'C0-',  label='Silver')
+    imaginary_part_axes.plot(silver_wavelengths/const.nano, silver_empirical_permittivity.imag,
                         'C0o')
     imaginary_part_axes.plot(
-        wavelengths, gold_permittivity.imag, 'C1-',  label='Gold')
-    imaginary_part_axes.plot(gold_wavelengths, gold_empirical_permittivity.imag,
+        wavelengths/const.nano, gold_permittivity.imag, 'C1-',  label='Gold')
+    imaginary_part_axes.plot(gold_wavelengths/const.nano, gold_empirical_permittivity.imag,
                         'C1o')
-    imaginary_part_axes.set_xlabel('Free space wavelength (m)')
+    imaginary_part_axes.set_xlabel('Free space wavelength (nm)')
     imaginary_part_axes.set_ylabel('Permittivity, imaginary part')
     imaginary_part_axes.legend()
 
