5b80ffb6f765eb12e57f7e00bf49e58dc7e2316d
george <george.duffett@icloud.com>
2023-03-12 22:11:45 +0000

    Add Jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,66 @@
+@Library('pipeline-lib') _
+
+pipeline {
+	
+	agent { label 'python' }
+	
+	stages {
+
+		stage('Install dependencies') {
+			steps {
+				sh "make dev_dependencies"
+			}
+		}
+
+		stage('Lint') {
+			steps {
+				script {
+					try {
+						sh "make lint"
+					} catch (error) {
+						unstable(message: "${STAGE_NAME} is unstable")
+					}
+				}
+			}
+		}
+
+		stage('Test') { 
+			steps {
+				script {
+					try {
+						sh "make test"
+					} catch (error) {
+						unstable(message: "${STAGE_NAME} is unstable")
+					}
+				}
+			}
+		}
+	}
+
+	post {
+
+		always {
+			sh "make clean"
+		}
+
+
+		success {
+			script {
+				notifyGitHubBuildStatus("optical_dispersion_relations", "success")
+			}
+		}
+
+		unstable {
+			script {
+				notifyGitHubBuildStatus("optical_dispersion_relations", "failure")
+			}
+		}
+
+		failure {
+			script {
+				notifyGitHubBuildStatus("optical_dispersion_relations", "error")
+			}
+		}
+
+	}
+}
