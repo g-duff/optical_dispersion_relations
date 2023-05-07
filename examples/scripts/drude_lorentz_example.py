7b7279d608dcc394f8a40457869c9e4deeff5341
george <george.duffett@icloud.com>
2023-05-07 02:10:21 +0100

    Change gold example to builder

--- examples/scripts/drude_lorentz_example.py
+++ examples/scripts/drude_lorentz_example.py
@@ -10,30 +10,14 @@ from optical_dispersion_relations import utilities
 
 matplotlib.rc('font', size=12)
 
-
-GOLD_DRUDE_PARAMETERS = {
-    'dielectric_constant': 6,
-    'plasma_frequency': 1,
-    'first_pole': {
-        'peak_strength': 5.37e15**2,
-        'damping_constant': 6.216e13,
-        'peak_position': 0,
-    },
-    'second_pole': {
-        'peak_strength': 2.263e15**2,
-        'damping_constant': 1.332e15,
-        'peak_position': 4.572e15
-    }
-}
-
 GOLD_FILEPATH = './empirical_data/gold.txt'
 SILVER_FILEPATH = './empirical_data/silver.txt'
 
 FILE_FORMAT = {
-        'unpack':True,
-        'skip_header':1,
-        'converters': { 0: lambda x: float(x)*const.micro }
-        }
+    'unpack': True,
+    'skip_header': 1,
+    'converters': {0: lambda x: float(x)*const.micro}
+}
 
 if __name__ == '__main__':
     wavelengths = np.arange(450, 1000, 1)*const.nano
@@ -41,13 +25,21 @@ if __name__ == '__main__':
     angular_frequency = 2*const.pi*const.speed_of_light/(wavelengths)
 
     silver_permittivity = drude_lorentz.DrudeLorentz().with_angular_frequency(
-            angular_frequency
+        angular_frequency
     ).with_plasma_frequency(1.35e16).add_pole(
-            damping_constant=0.0023*1.35e16
+        damping_constant=0.0023*1.35e16
     ).permittivity()
 
-    gold_permittivity = drude_lorentz.double_pole(
-        angular_frequency, **GOLD_DRUDE_PARAMETERS)
+    gold_permittivity = drude_lorentz.DrudeLorentz().with_dielectric_constant(
+        6
+    ).with_angular_frequency(angular_frequency).add_pole(
+        peak_strength=6*5.37e15**2,
+        damping_constant=6.216e13,
+    ).add_pole(
+        peak_strength=6*2.263e15**2,
+        damping_constant=1.332e15,
+        peak_position=4.572e15
+    ).permittivity()
 
     silver_wavelengths, silver_refractive_index, silver_extinction_coefficient = \
         np.genfromtxt(SILVER_FILEPATH, **FILE_FORMAT)
@@ -77,15 +69,14 @@ if __name__ == '__main__':
     imaginary_part_axes.plot(
         wavelengths/const.nano, silver_permittivity.imag, 'C0-',  label='Silver')
     imaginary_part_axes.plot(silver_wavelengths/const.nano, silver_empirical_permittivity.imag,
-                        'C0o')
+                             'C0o')
     imaginary_part_axes.plot(
         wavelengths/const.nano, gold_permittivity.imag, 'C1-',  label='Gold')
     imaginary_part_axes.plot(gold_wavelengths/const.nano, gold_empirical_permittivity.imag,
-                        'C1o')
+                             'C1o')
     imaginary_part_axes.set_xlabel('Free space wavelength (nm)')
     imaginary_part_axes.set_ylabel('Permittivity, imaginary part')
     imaginary_part_axes.legend()
 
     fig.tight_layout()
     fig.savefig('../images/drude_lorentz_example.png')
-
