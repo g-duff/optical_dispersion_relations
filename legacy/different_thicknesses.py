ee30dfb0336fe4434f1ade326eab5eb993662eaf
george <george.duffett@icloud.com>
2023-07-03 00:14:50 +0100

    fix: different thicknesses legacy example

--- legacy/different_thicknesses.py
+++ legacy/different_thicknesses.py
@@ -1,79 +1,78 @@
 import numpy as np
-import scipy.constants as spc
+import scipy.constants as const
 import scipy.interpolate as interp
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
 
-from optical_dispersion_relations import plasmon
+from optical_dispersion_relations import plasmon, utilities
+
 
 ## Wavelength range
-wl = 1/np.linspace(1.0/1500, 1.0/350, 100)
-# wl = np.linspace(300,1500,100)
+wavelengths = 1/np.linspace(1.0/1500, 1.0/350, 100)*const.nano
 
 # Insulator RI and thickness
-i_n = 1.45
-t_i = 20e-9
+dielectric_refractive_index = 1.45
+thickness = 20*const.nano
 
 ## Metal refractive index and extinction coefficient
-m_wl, m_n, = np.genfromtxt('./material_dispersions/ag_n.txt',
+metal_wavelengths, metal_refractive_index, = np.genfromtxt('./material_dispersions/ag_n.txt',
 	unpack=True, skip_header=1)
-m_k = np.genfromtxt('./material_dispersions/ag_k.txt',
+metal_extinction_coefficient = np.genfromtxt('./material_dispersions/ag_k.txt',
 	unpack=True, skip_header=1, usecols=(1))
-
-# Convert units
-m_wl = m_wl*1e-6	# um to m
-wl = wl*1e-9		# nm to m
+metal_wavelengths = metal_wavelengths*const.micro
 
 ## Calculate wavevector, frequency, energy
-k0 = 2*np.pi/wl
-om = k0*spc.c
-ev = om*spc.hbar/spc.e
-THz = om/(2*np.pi*1e12)
-ang_THz = om/(1e15)
+wavenumbers = 2*const.pi/wavelengths
+angular_frequency = wavenumbers*const.c
 
 ## Interpolate metal n and k to match user wavelength range
-m_n = interp.griddata(m_wl, m_n, wl, method='cubic')
-m_k = interp.griddata(m_wl, m_k, wl, method='cubic')
+metal_refractive_index = interp.griddata(metal_wavelengths, metal_refractive_index, wavelengths, method='cubic')
+metal_extinction_coefficient = interp.griddata(metal_wavelengths, metal_extinction_coefficient, wavelengths, method='cubic')
 
 ## Convert RI to permittivity
-m_eps, i_eps = (m_n + 1j*m_k)**2, i_n**2
+metal_permittivities = utilities.refractive_index_to_permittivity(metal_refractive_index + 1j*metal_extinction_coefficient)
+dielectric_permittivity = utilities.refractive_index_to_permittivity(dielectric_refractive_index).real
 
 ## Calcualte spp neff
-spp_n_eff = plasmon.surface_plasmon_polariton(i_eps, m_eps)
-spp_beta = spp_n_eff*k0
+surface_plasmon_polariton_effective_index = plasmon.surface_plasmon_polariton(
+        dielectric_permittivity, 
+        metal_permittivities
+)
+surface_plasmon_polariton_propagation_constant = surface_plasmon_polariton_effective_index*wavenumbers
 
-k_light_line = i_n*k0
-
-y = ang_THz
+y = angular_frequency
 
 disp_fig, disp_ax = plt.subplots()
-disp_ax.plot(spp_beta.real, y, label='SPP')
-disp_ax.plot(k_light_line, y, 'k--', label='Light line')
+disp_ax.plot(surface_plasmon_polariton_propagation_constant.real, y, label='SPP')
+disp_ax.plot(dielectric_refractive_index*wavenumbers, y, 'k--', label='Light line')
 
 index_fig, index_ax = plt.subplots()
 
-index_ax.plot(wl*1e9, spp_n_eff.real, label='SPP')
-index_ax.plot(wl*1e9, i_n*np.ones(len(wl)), 'k--', label='Insulator')
+index_ax.plot(wavelengths*1e9, surface_plasmon_polariton_effective_index.real, label='SPP')
+index_ax.plot(wavelengths*1e9, dielectric_refractive_index*np.ones(len(wavelengths)), 'k--', label='Insulator')
 
 # Starting estimate for Newton-Raphson
-newt = spp_beta
+newt = surface_plasmon_polariton_propagation_constant
 
-for t_i in [100e-9, 50e-9, 40e-9, 30e-9, 25e-9, 20e-9]:
+for thickness in [100e-9, 50e-9, 40e-9, 30e-9, 25e-9, 20e-9]:
 
-	# Newton-Raphson process
-	newt_args = zip(newt, k0, m_eps)
-	newt = np.array([
-		opt.newton(
+    # Newton-Raphson process
+    initial_guesses = newt
+    newt = np.array([
+        opt.newton(
             func=plasmon.transcendential_trilayer_even_magnetic_field,
-            x0=a[0],
-            args=(2*np.pi/a[1], t_i, i_eps, a[2]),
-		    maxiter=int(1e6),
+            x0=initial_guess,
+            args=(wavelength, thickness, dielectric_permittivity, metal_permittivity),
+            maxiter=int(1e6),
             tol=1e3
-        ) for a in newt_args])
+        ) for initial_guess, wavelength, metal_permittivity in
+        zip(initial_guesses, wavelengths, metal_permittivities)
+    ])
 
-	# Plot dispersion relation and effective index
-	disp_ax.plot(newt.real, y, lw=2, label=str(t_i*1e9)+'nm')
-	index_ax.plot(wl*1e9, newt.real/k0, label=str(t_i*1e9)+'nm')
+    # Plot dispersion relation and effective index
+    thickness_in_nm = round(thickness / const.nano)
+    disp_ax.plot(newt.real, y, lw=2, label=f'{thickness_in_nm} nm')
+    index_ax.plot(wavelengths/const.nano, newt.real/wavenumbers, label=f'{thickness_in_nm} nm')
 
 disp_ax.set_xlabel(r'Propagation constant $\beta$')
 disp_ax.set_ylabel(r'Energy (eV)')
